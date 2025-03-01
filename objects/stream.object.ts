07165357ec47f3f57b550185f4c1e68ce8150c8d
kevingeronimo <194843499+kevingeronimo@users.noreply.github.com>
2025-03-01 15:51:09 -0500

    chore: prepare stream object repr

--- objects/stream.object.ts
+++ objects/stream.object.ts
@@ -1,3 +1,14 @@
+import type { ObjCounter } from "../utils/obj-counter.util.ts";
 import { IndirectObject } from "./indirect.object.ts";
 
-export class ObjectStream extends IndirectObject {}
+export class StreamObject extends IndirectObject {
+  constructor(objCounter: ObjCounter) {
+    super(objCounter);
+  }
+
+  override toString(): string {
+    const dictionary = "<< /Length 0 >>";
+    const stream = "stream\nendstream";
+    return this.withLabel(`${dictionary}\n${stream}`);
+  }
+}
